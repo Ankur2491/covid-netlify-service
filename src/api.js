ccc07ff9d68d295d2f61502ddad22c8cc9f1537f
ankur2491 <ankur.101045@gmail.com>
2020-07-15 02:38:45 +0530

    added axios

--- src/api.js
+++ src/api.js
@@ -2,14 +2,18 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const request = require('request');
+const axios = require('axios');
 const router = express.Router();
-
+app.set('json spaces', 0);
 router.get('/worldData',(req,res)=>{
-    request('https://2019ncov.asia/api/country_region',(error,response,body)=>{
-        if(!error && response.statusCode == 200){
-          res.send(body);
-        }
-      })
+    // request('https://2019ncov.asia/api/country_region',(error,response,body)=>{
+    //     if(!error && response.statusCode == 200){
+    //       res.send(body);
+    //     }
+    //   })
+    axios.get('https://2019ncov.asia/api/country_region')
+    .then(resp=> res.json(resp.data))
+    .catch(err=> {console.log(err)});
 });
 
 router.get('/countryData/:country',(req,res)=>{
